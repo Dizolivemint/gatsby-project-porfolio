1840569f44d7a1eed7f3b894fac523686ee89cfd
Miles Exner <dizolivemint@gmail.com>
2022-05-22 11:42:36 -0700

    Update config dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,14 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-image`,
+    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-source-graphcms`,
       options: {
         endpoint: process.env.GRAPHCMS_ENDPOINT,
       },
     },
+    'gatsby-transformer-sharp',
   ],
 };
